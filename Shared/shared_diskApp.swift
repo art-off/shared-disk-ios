6151424584d705d6da1d1427a103e55966672968
art-off <a.rylov@id-east.ru>
2021-05-22 16:00:35 +0700

    Initial Commit

---
+++ Shared/shared_diskApp.swift
@@ -0,0 +1,17 @@
+//
+//  shared_diskApp.swift
+//  Shared
+//
+//  Created by Artem Rylov on 22.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct shared_diskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
